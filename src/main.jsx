b062a5935c4aa94421a7396904bbd52b4f663355
amitk9627 <ramitgzp56@gmail.com>
2025-05-04 00:33:21 +0530

    login merge fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store, persistor } from "./lib/store/store.js";
 // import { PersistGate } from 'redux-persist/lib/integration/react';
-import { PersistGate } from "redux-persist/integration/react";
+// import { PersistGate } from "redux-persist/integration/react";
+
+import { PersistGate } from 'redux-persist/es/integration/react';
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
