1b180eff54eb04fc7cf0ce93261d7f63c66f1047
Amit kumar <120603787+amitk9627@users.noreply.github.com>
2025-05-07 09:59:36 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const backendUrl="https://eduauraabackend.up.railway.app";
-export const backendUrl="http://localhost:5000";
+ export const backendUrl="https://eduauraabackend.up.railway.app";
+// export const backendUrl="http://localhost:5000";
